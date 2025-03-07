f383d13834812a94278ada453f19bc323cfb5e2c
POLETTI28 <fernando_poletti28@hotmail.com>
2025-03-07 19:09:47 -0300

    Add files via upload

---
+++ DATA LAB2.R
@@ -0,0 +1,54 @@
+# AULA 2 DATA LAB
+
+# TIRAR NOTAÇÃO CIENTÍFICA ####
+options(scipen = 999)
+
+# EXCLUIR UMA BASE DE DADOS
+nomedoobjeto <- NULL
+
+## ATALHOS DE TECLADO ####
+# "<-": "ALT" + "-"
+# PARA EXECUTAR A LINHA: "ALT" + "ENTER"
+# PARA EXECUTAR TODO SCRIPT: "ALT" + "SHIFT" + "ENTER"
+
+## BIBLIOTECA PARA ACESSAR DADOS DO BANCO MUNDIAL ####
+
+#install.packages('WDI') # PRIMEIRO A EXECUTAR
+library(WDI) # CARREGAR O PACOTE
+
+## VARIÁVEIS ####
+
+# PIB A PREÇOS CORRENTES (US$): NY.GDP.MKTP.CD
+# DESPESAS DE CONSUMO PESSOAL, PC/US$: NE.CON.PRVT.CD
+
+# Infos: http://vincentarelbundock.github.io/WDI/
+
+PIB_vars <- WDIsearch('gdp') # GDP = PIB (todos)
+TODO_BM <- WDIsearch('') # TODAS AS VAR. DO BM
+
+variaveis <- c('NE.EXP.GNFS.ZS',
+               'NE.IMP.GNFS.ZS')
+
+# BASE DE DADOS DE CORTE TRANSVERSAL
+
+dados <- WDI(indicator = variaveis,
+             country = 'all', # TODOS = "all"
+             start = 2022, end = 2022) # PROBLEMA DE NA
+
+#install.packages("tidyverse")
+library(tidyverse)
+dadosok <- WDI(indicator = variaveis,
+               country = 'all', # TODOS = "all"
+               start = 2022, end = 2022) %>% 
+  na.omit()
+
+# BASE DE DADOS DA APRESENTAÇÃO (BRASIL)
+
+dadosbr <- WDI(indicator = variaveis,
+               country = 'BR', # Código Iso2C
+               start = 1961, end = 2018)
+
+# BASE COMPLETA DO BRASIL
+
+dadosbrcomp <- WDI(indicator = variaveis,
+                   country = 'BR')
